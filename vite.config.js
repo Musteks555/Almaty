6c159a4f3b92652c638698e8bad1e84f54826400
Artem Maliarov <artemmustek73@gmail.com>
2025-03-18 18:39:45 +0200

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite';
-// import glob from 'glob';
-import { globSync } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
+import path from "path";
+import { globSync } from "glob";
 
-export default defineConfig({
-   define: {
-    global: {},
-  },
-  root: 'src',
-  build: {
-    rollupOptions: {
-    //   input: glob.sync('./src/*.html'),
-    input: globSync('./src/*.html')
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    root: "src",
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: globSync("./src/*.html").reduce((acc, file) => {
+          acc[path.basename(file, ".html")] = file;
+          return acc;
+        }, {}),
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+          entryFileNames: "[name].js",
+        },
+      },
+      outDir: "../dist",
+    },
+    plugins: [injectHTML(), FullReload(["./src/**/*.html"])],
+  };
 });
